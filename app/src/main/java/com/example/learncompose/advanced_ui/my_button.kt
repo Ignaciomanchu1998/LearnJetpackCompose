769fdd657ed26bfb2ca73bbc437eb7162e8e61f2
Ignacio98 <dani.manchu1998@gmail.com>
2024-10-28 22:41:46 -0500

    Adding files for create to learn jetpack compose

---
+++ app/src/main/java/com/example/learncompose/advanced_ui/my_button.kt
@@ -0,0 +1,29 @@
+package com.example.learncompose.advanced_ui
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyButton(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { Log.i("Button Clicked", "MyButton") },
+        ) {
+            Text("Click me")
+        }
+
+        Text("Hello world")
+    }
+}
---
+++ app/src/main/java/com/example/learncompose/advanced_ui/my_button_state.kt
@@ -0,0 +1,35 @@
+package com.example.learncompose.advanced_ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyButtonState(modifier: Modifier = Modifier) {
+
+    var enabled by rememberSaveable { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Adding Button - State")
+
+        OutlinedButton(
+            onClick = { enabled = false },
+            enabled = enabled,
+        ) {
+            Text("Click Me - Here")
+        }
+    }
+}
